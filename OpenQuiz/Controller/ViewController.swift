2db660b83eed53f7ced7c34f09c0c2cf51694b6a
Siarhei Saukin <saukin@yahoo.com>
2021-05-13 17:17:55 -0400

    first

---
+++ OpenQuiz/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenQuiz
+//
+//  Created by Siarhei Saukin (Étudiant) on 2021-05-13.
+//  Copyright © 2021 Siarhei Saukin (Étudiant). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
