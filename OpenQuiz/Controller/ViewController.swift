0088a3e7d209404a24b8c06330f379e15e29810e
Siarhei Saukin <saukin@yahoo.com>
2021-05-20 18:04:08 -0400

    interface made

--- OpenQuiz/Controller/ViewController.swift
+++ OpenQuiz/Controller/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blue: UIView!
+    
+    @IBOutlet weak var white: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
     }
 
 
+    
 }
 
